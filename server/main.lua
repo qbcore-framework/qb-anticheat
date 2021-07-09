b890b40c24a3a4a391a2f85bffd8cfec7584d250
GhzGarage <gigahertzgarage@mail.com>
2021-07-08 19:55:23 -0500

    ⚠️⚠️ Steam Ditch ⚠️⚠️

--- server/main.lua
+++ server/main.lua
@@ -10,9 +10,17 @@ end)
 RegisterServerEvent('qb-anticheat:server:banPlayer')
 AddEventHandler('qb-anticheat:server:banPlayer', function(reason)
     local src = source
-    TriggerClientEvent('chatMessage', -1, "QB Anti-Cheat", "error", GetPlayerName(src).." is verbannen voor: " ..reason )
-    exports.ghmattimysql:execute("INSERT INTO `bans` (`name`, `steam`, `license`, `discord`,`ip`, `reason`, `expire`, `bannedby`) VALUES ('"..GetPlayerName(src).."', '"..GetPlayerIdentifiers(src)[1].."', '"..GetPlayerIdentifiers(src)[2].."', '"..GetPlayerIdentifiers(src)[3].."', '"..GetPlayerIdentifiers(src)[4].."', '"..reason.."', 2145913200, '"..GetPlayerName(src).."')")
-    DropPlayer(src, "You have been flagged too many times by our anticheat and prevented from being banned from the server. Check our discord for more information: https://discord.gg/cYwKMKX")
+    TriggerEvent("qb-log:server:CreateLog", "anticheat", "Anti-Cheat", "white", GetPlayerName(src).." Has Been Banned For "..reason, false)
+    exports.ghmattimysql:execute('INSERT INTO bans (name, license, discord, ip, reason, expire, bannedby) VALUES (@name, @license, @discord, @ip, @reason, @expire, @bannedby)', {
+        ['@name'] = GetPlayerName(src),
+        ['@license'] = QBCore.Functions.GetIdentifier(src, 'license'),
+        ['@discord'] = QBCore.Functions.GetIdentifier(src, 'discord'),
+        ['@ip'] = QBCore.Functions.GetIdentifier(src, 'ip'),
+        ['@reason'] = reason,
+        ['@expire'] = 2145913200,
+        ['@banneby'] = 'Anti-Cheat'
+    })
+    DropPlayer(src, "You Have Been Banned For Cheating. Contact Staff (or dont): https://discord.gg/")
 end)
 
 -- Fake events --
