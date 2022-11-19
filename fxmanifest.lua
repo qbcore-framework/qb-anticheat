97de7f4d0a5eff7d92f1331bc342b9980759272f
Charlie <dev@charlie.fyi>
2022-11-19 12:48:25 +0000

    Bump version / tidy

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,13 +2,15 @@ fx_version 'cerulean'
 game 'gta5'
 
 description 'QB-Anticheat'
-version '1.0.0'
+version '1.2.0'
 
 shared_script 'config.lua'
+
 client_script 'client/main.lua'
+
 server_scripts {
-	'@oxmysql/lib/MySQL.lua',
-	'server/main.lua'
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
 }
 
 lua54 'yes'
